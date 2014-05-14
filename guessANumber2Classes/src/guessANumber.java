67ab7b342799bd33a225a13d0b5b0f11e2217b82
Jorge Catalan <jesus@192.168.1.123>
2014-05-13 21:22:39 -0700

    a

---
+++ guessANumber2Classes/src/guessANumber.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class guessANumber {
+		
+	int game = 0 + (int) (Math.random() * 10);
+	
+	
+	
+	public void displayMessage()
+	{
+		System.out.println("The system choose " + game);
+			
+	}
+	
+}
+	
+	
+	
+	
---
+++ guessANumber2Classes/src/guessANumberTest.java
@@ -0,0 +1,18 @@
+
+public class guessANumberTest {
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub		
+		
+		guessANumber myGuessNumber = new guessANumber();
+		
+		myGuessNumber.displayMessage();
+
+	}
+
+}
+
+
+		
+
+	
